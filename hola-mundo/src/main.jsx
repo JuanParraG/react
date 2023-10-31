cba3f4cdfbd1c8e4281e398ff44232c7b2d26659
JuanParraG <juadparrag@gmail.com>
2023-10-31 17:53:26 -0500

    nuevo parrafo

--- hola-mundo/src/main.jsx
+++ hola-mundo/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <p>holla</p>
+    <p>holla sss</p>
+    <p>dfsf</p>
   </React.StrictMode>,
 )
